c6a9fc9116fdbaba81a4291dbb0fd84d2fd76615
ash-krish <aishu1188@gmail.com>
2019-11-14 20:55:44 +1100

    updated with dishes

--- SuperEats.rb
+++ SuperEats.rb
@@ -1,9 +1,8 @@
-cuisine = ["Italian", "Burgers and Shakes", "Indian"]
+cuisine = ["Italian", "Mexican", "Indian"]
 
 italian = {"Margherita Pizza" => 14, "BBQ Chicken" => 17, "Bolognese Pasta" => 19, "Basil Pasta" => 18, "Pumpkin Risotto" => 20, "Garlic Bread" => 7}
-burger_and_shakes = {"Pork Belly" => 13, "Crispy chicken" => 12, "Mushroom burger" => 11, "Onion rings" => 6, "Caramel Shake" => 8, "Chocolate Shake" => 8, "Vanilla Shake" => 8}
-indian = {"Butter chicken" => 20, "Lamb Biriyani" => 18, "Chicken Kofta" => 21, "Dal Tadka" => 16, "Naan" => 5}
-
+mexican = {"Veggie nachos" => 13, "Chicken tacos" => 9, "Burritos" => 11, "Stuffed Jalapenos" => 6, "Quesadilla" => 13, "Jarritos soda" => 8, "Warm churros" => 8}
+indian = {"Butter chicken" => 20, "Lamb Biriyani" => 18, "Chicken Kofta" => 21, "Dal Tadka" => 16, "Naan" => 5, "Mango lassi" => 4}
 
 puts "WELCOME TO SUPER EATS"
 puts "Please enter you username"
@@ -19,27 +18,25 @@ end
 
 cuisine_type = gets.chomp
 puts
-if cuisine_type = italian
+if (cuisine_type == "Italian" || cuisine_type == "italian")
+    
     puts "Please choose from our sensational Italian menu."
-
-cuisine_type = italian 
+ 
 italian.each_key do |key|
         puts key
-end
-
-
+    end
 puts
-elsif cuisine_type = burger_and_shakes
-puts "Please select from our Burger & Shakes menu"
-burger_and_shakes.each_key do |key|
+elsif (cuisine_type == "Mexican" || cuisine_type == "mexican")
+puts "Please select from our sassy Mexican menu"
+mexican.each_key do |key|
     puts key
 end
-
 puts
-elsif cuisine_type = indian
+elsif (cuisine_type == "Indian" || cuisine_type == "indian")
 puts "Please select from our finger-licking Indian menu"
 indian.each_key do |key|
     puts key
 end
 else puts "Please enter a valid option"
 end
+  
